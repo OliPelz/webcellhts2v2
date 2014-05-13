f6e88d2e6a39edc736756488f10e6f3e777f0e2e
Oliver Pelz <oliverpelz@googlemail.com>
2014-05-13 16:29:44 +0200

    created and added a basic server structure, built with grails 2.3.8

---
+++ server/webcellhts2v2-server/test/unit/webcellhts2v2/server/DataFilesControllerSpec.groovy
@@ -0,0 +1,20 @@
+package webcellhts2v2.server
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(DataFilesController)
+class DataFilesControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
